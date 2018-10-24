6388223bbdd3881ed751b33823dc57c2044c1359
Greg <gregoryamitten@gmail.com>
2018-10-24 19:00:40 +0100

    More packaging changes

--- Back-End/src/ase-group-1/server/controllers/GeolocationController.java
+++ Back-End/src/ase-group-1/server/controllers/GeolocationController.java
@@ -1,4 +1,4 @@
-package ase.controller;
+package server.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
