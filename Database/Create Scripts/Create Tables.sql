15c6f319e43d36e42dc71707227cd571dbeb836c
Rikkey <rikkeypaal@gmail.com>
2018-10-24 19:21:28 +0100

    Created database init scripts and initial database folder structure

---
+++ Database/Create Scripts/Create Tables.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS AppDB;
+
+USE AppDB;
+
+CREATE TABLE IF NOT EXISTS Users (
+	UserID CHAR(32),
+    
+	PRIMARY KEY(UserID)
+);
+
+
+CREATE TABLE IF NOT EXISTS Location (
+	UserID CHAR(32),
+	Timelog DATETIME,
+	Longitude FLOAT,
+	Latitude FLOAT,
+	Altitude FLOAT,
+	Delivered BOOLEAN,
+
+	PRIMARY KEY(UserID, Timelog),
+	FOREIGN KEY(UserID) REFERENCES Users(UserID)
+	-- Delete all users's locations if user is deleted
+		ON DELETE CASCADE
+	-- Dont allow user to update ID if has locations
+		ON UPDATE RESTRICT
+);
