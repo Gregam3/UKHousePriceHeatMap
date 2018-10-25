74fcfe2187c80ca38fce73da5dc6488ff1ce9893
James Fernando <jamesfernando@hotmail.co.uk>
2018-10-25 13:54:34 +0100

    Added work on controllers

--- Spring-Boot/src/main/ase/controller/UserController.java
+++ Spring-Boot/src/main/ase/controller/UserController.java
@@ -3,6 +3,7 @@ package main.ase.controller;
 import main.ase.DataEntities.User;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class UserController {
   @RequestMapping(value = "/user", method = RequestMethod.POST)
   public @ResponseBody String post(@RequestBody User user) {
