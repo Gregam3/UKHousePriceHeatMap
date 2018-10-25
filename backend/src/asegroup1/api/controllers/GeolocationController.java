1d29a8e1571a9f235a30dce1973fa2911dde6aeb
gregoryamitten@gmail.com <gregoryamitten@gmail.com>
2018-10-25 16:36:18 +0100

    Added test sources folder in backend root

--- backend/src/asegroup1/api/controllers/GeolocationController.java
+++ backend/src/asegroup1/api/controllers/GeolocationController.java
@@ -1,12 +1,13 @@
-package api.controllers;
+package asegroup1.api.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class GeolocationController {
-  @RequestMapping("/")
-  public String locationMock() {
-    return "[EXAMPLE COORDINATES]";
-  }
+
+    @RequestMapping("/")
+    public String locationMock() {
+        return "[EXAMPLE COORDINATES]";
+    }
 }
