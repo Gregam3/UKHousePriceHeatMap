308ec2df9cdeda4373a3ddf4af3a77a8a9578f31
Unknown <jamesfernando@hotmail.co.uk>
2018-10-28 18:33:13 +0000

    Added more work on location controller

--- backend/src/main/java/asegroup1/api/controllers/LocationController.java
+++ backend/src/main/java/asegroup1/api/controllers/LocationController.java
@@ -1,7 +1,7 @@
 package asegroup1.api.controllers;
 
-import asegroup1.api.models.UserData;
-import asegroup1.api.services.user.UserServiceImpl;
+import asegroup1.api.models.LocationData;
+import asegroup1.api.services.location.LocationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +23,8 @@ public class LocationController {
 
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody
-	String post(@RequestBody UserData user, @RequestBody LocationData location) {
-		if ()
-		return "Successful Response";
+	String post(@RequestBody LocationData location) {
+		locationService.create(location)
+		return new ResponseEntity<String>("successfully added to databse", HttpStatus.OK);
 	}
 }
