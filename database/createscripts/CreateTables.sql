93c0027cbc6a7263283e60f39f93f1f53e980b94
Unknown <jamesfernando@hotmail.co.uk>
2018-10-30 20:00:07 +0000

    updated create scripts to current values

--- database/createscripts/CreateTables.sql
+++ database/createscripts/CreateTables.sql
@@ -1,24 +1,25 @@
-CREATE DATABASE IF NOT EXISTS AppDB;
+-- Set these values
+CREATE DATABASE IF NOT EXISTS development;
 
-USE AppDB;
+USE development;
 
-CREATE TABLE IF NOT EXISTS Users (
-	UserID CHAR(32),
+CREATE TABLE IF NOT EXISTS user_data (
+	USER_ID CHAR(64),
     
-	PRIMARY KEY(UserID)
+	PRIMARY KEY(USER_ID)
 );
 
 
-CREATE TABLE IF NOT EXISTS Locations (
-	UserID CHAR(32),
-	Timelog DATETIME,
-	Longitude FLOAT,
-	Latitude FLOAT,
-	Altitude FLOAT,
-	Delivered BOOLEAN,
+CREATE TABLE IF NOT EXISTS location_data (
+	USER_ID CHAR(64),
+	TIMELOG DATETIME,
+	LONGITUDE FLOAT,
+	LATITUDE FLOAT,
+	ALTITUDE FLOAT,
+	DELIVERED BOOLEAN,
 
-	PRIMARY KEY(UserID, Timelog),
-	FOREIGN KEY(UserID) REFERENCES Users(UserID)
+	PRIMARY KEY(USER_ID, TIMELOG),
+	FOREIGN KEY(USER_ID) REFERENCES user_data(USER_ID)
 	-- Delete all users's locations if user is deleted
 		ON DELETE CASCADE
 	-- Dont allow user to update ID if has locations
