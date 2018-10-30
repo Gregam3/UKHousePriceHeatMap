0733d86f3f07e64774d879f8a449bf934bb5e2ad
Greg <gregoryamitten@gmail.com>
2018-10-30 22:02:21 +0000

    removed redundant import

--- backend/src/main/java/asegroup1/api/services/ServiceImpl.java
+++ backend/src/main/java/asegroup1/api/services/ServiceImpl.java
@@ -1,6 +1,5 @@
 package asegroup1.api.services;
 
-//import asegroup1.api.daos.Dao;
 import asegroup1.api.daos.DaoImpl;
 
 import java.util.List;
@@ -11,47 +10,47 @@ import java.util.List;
  */
 
 public class ServiceImpl<T> {
-	private DaoImpl<T> dao;
+    private DaoImpl<T> dao;
 
-	public ServiceImpl(DaoImpl<T> dao) {
-		this.dao = dao;
-	}
+    public ServiceImpl(DaoImpl<T> dao) {
+        this.dao = dao;
+    }
 
-	public T get(String id) {
-		checkIfDaoIsValid();
-		return dao.get(id);
-	}
+    public T get(String id) {
+        checkIfDaoIsValid();
+        return dao.get(id);
+    }
 
-	public void update(T t) {
-		checkIfDaoIsValid();
-		dao.update(t);
-	}
+    public void update(T t) {
+        checkIfDaoIsValid();
+        dao.update(t);
+    }
 
-	public void delete(String id) {
-		checkIfDaoIsValid();
-		dao.delete(id);
-	}
+    public void delete(String id) {
+        checkIfDaoIsValid();
+        dao.delete(id);
+    }
 
-	public void delete(T t) {
-		checkIfDaoIsValid();
-		dao.delete(t);
-	}
+    public void delete(T t) {
+        checkIfDaoIsValid();
+        dao.delete(t);
+    }
 
-	public List<T> list() {
-		checkIfDaoIsValid();
-		return dao.list();
-	}
+    public List<T> list() {
+        checkIfDaoIsValid();
+        return dao.list();
+    }
 
     public void create(T t) {
         checkIfDaoIsValid();
         dao.add(t);
     }
 
-	private void checkIfDaoIsValid() {
-		if (dao == null) {
-			throw new AssertionError("In order to use this method you must set the class in constructor. " +
-					"E.g. for UserServiceImpl extends ServiceImpl<User>, you should put super(userDao) in your constructor." +
-					"If userDao does not yet exist you must create it and then @Autowire it to give it its state");
-		}
-	}
+    private void checkIfDaoIsValid() {
+        if (dao == null) {
+            throw new AssertionError("In order to use this method you must set the class in constructor. " +
+                    "E.g. for UserServiceImpl extends ServiceImpl<User>, you should put super(userDao) in your constructor." +
+                    "If userDao does not yet exist you must create it and then @Autowire it to give it its state");
+        }
+    }
 }
