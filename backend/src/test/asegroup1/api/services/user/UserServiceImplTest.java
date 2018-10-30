65a59403853db0f408448cdbf63b49830ccd03ac
Greg <gregoryamitten@gmail.com>
2018-10-30 18:54:21 +0000

    Fixed unit test

--- backend/src/test/asegroup1/api/services/user/UserServiceImplTest.java
+++ backend/src/test/asegroup1/api/services/user/UserServiceImplTest.java
@@ -1,31 +1,35 @@
 package asegroup1.api.services.user;
 
 import asegroup1.api.controllers.UserController;
+import asegroup1.api.daos.user.UserDaoImpl;
 import asegroup1.api.models.UserData;
+import asegroup1.api.services.user.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class UserServiceImplTest {
+class UserControllerImplTest {
 
     @Test
     void testIfUserControllerSendsRequestThroughToDao() {
-//        String userId = "test";
-//
-//        UserData userData = new UserData();
-//        userData.setUserId(userId);
-//
-//        UserDao userDaoMock = mock(UserDao.class);
-//        when(userDaoMock.get(userId)).thenReturn(userData);
-//
-//
-//        UserServiceImpl userService = new UserServiceImpl(userDaoMock);
-//        UserController userController = new UserController(userService);
-//
-//        ResponseEntity<UserData> response = userController.get("test");
-//
-//        assert (response.getStatusCode().is2xxSuccessful() &&  userController.get("test").getBody().getUserId().equals(userId));
+        String userId = "test";
+
+        UserData userData = new UserData();
+        userData.setUserId(userId);
+
+        UserDaoImpl userDaoMock = mock(UserDaoImpl.class);
+        when(userDaoMock.get(userId)).thenReturn(userData);
+
+
+        UserServiceImpl userService = new UserServiceImpl(userDaoMock);
+        UserController userController = new UserController(userService);
+
+        ResponseEntity<UserData> response = userController.getUserData("test");
+
+        assert (response.getStatusCode().is2xxSuccessful() &&  Objects.requireNonNull(userController.getUserData("test").getBody()).getUserId().equals(userId));
     }
 }
