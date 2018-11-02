1c56b7f7e0710a8b3ab125846a4824ca8d86952b
Greg <gregoryamitten@gmail.com>
2018-11-02 11:50:21 +0000

    Transaction searching by post code implemented

--- backend/src/main/java/asegroup1/api/controllers/LandRegistryController.java
+++ backend/src/main/java/asegroup1/api/controllers/LandRegistryController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 
 @RestController
 @RequestMapping("/land-registry/")
@@ -23,15 +25,25 @@ public class LandRegistryController {
         this.landRegistryService = landRegistryService;
     }
 
-    @GetMapping("get/{post-code}")
-    public ResponseEntity<?> getLandRegistryDataForPostCode(@PathVariable("post-code") String postCode) {
-        if (!postCode.contains(" ")) {
-            return new ResponseEntity<>("Post code must contain a space", HttpStatus.BAD_REQUEST);
+    @GetMapping("get-addresses/{post-code}")
+    public ResponseEntity<?> getAddressDataForPostCode(@PathVariable("post-code") String postCode) {
+        //Checks if postcode has space in correct place in order to avoid error when it is split later on
+        if (postCode.charAt(postCode.length() - 4) != 32) {
+            return new ResponseEntity<>("Post code must contain a space in the correct position and be in a valid format http://www.restore.ac.uk/geo-refer/38330mtuks00y19740000.php", HttpStatus.BAD_REQUEST);
         }
 
         try {
             return new ResponseEntity<>(landRegistryService.getLandRegistryDataByPostCode(postCode), HttpStatus.OK);
-        } catch (UnirestException e) {
+        } catch (UnirestException | IOException e) {
+            return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("get-transactions/{post-code}")
+    public ResponseEntity<?> getTransactionDataForPostCode(@PathVariable("post-code") String postCode) {
+        try {
+            return new ResponseEntity<>(landRegistryService.getTransactionsForPostCode(postCode), HttpStatus.OK);
+        } catch (IOException | UnirestException e) {
             return new ResponseEntity<>(e, HttpStatus.BAD_REQUEST);
         }
     }
--- backend/src/main/java/asegroup1/api/services/landregistry/LandRegistryServiceImpl.java
+++ backend/src/main/java/asegroup1/api/services/landregistry/LandRegistryServiceImpl.java
@@ -1,15 +1,19 @@
 package asegroup1.api.services.landregistry;
 
+import asegroup1.api.models.HouseTransactionData;
 import asegroup1.api.models.LandRegistryData;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 @Service
@@ -17,9 +21,10 @@ import java.util.regex.Pattern;
 public class LandRegistryServiceImpl {
 
     private static final String LAND_REGISTRY_ROOT_URL = "http://landregistry.data.gov.uk/data/ppi/";
+    private static final String LAND_REGISTRY_SPARQL_ENDPOINT = "http://landregistry.data.gov.uk/app/root/qonsole/query";
     private static final String SPACE = "%20";
 
-    public List<LandRegistryData> getLandRegistryDataByPostCode(String postCode) throws UnirestException {
+    public List<LandRegistryData> getLandRegistryDataByPostCode(String postCode) throws UnirestException, IOException {
         List<LandRegistryData> landRegistryDataList = new LinkedList<>();
         String[] postCodeSplit = postCode.split(" ");
 
@@ -44,5 +49,60 @@ public class LandRegistryServiceImpl {
         return landRegistryDataList;
     }
 
+    public ArrayNode getTransactionsForPostCode(String postcode) throws IOException, UnirestException {
+        //TODO find better way to store query string
+        String postcodeQuery = "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "prefix skos: <http://www.w3.org/2004/02/skos/core#>\n" +
+                "prefix lrcommon: <http://landregistry.data.gov.uk/def/common/>\n" +
+                "prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
+                "prefix sr: <http://data.ordnancesurvey.co.uk/ontology/spatialrelations/>\n" +
+                "prefix ukhpi: <http://landregistry.data.gov.uk/def/ukhpi/>\n" +
+                "prefix lrppi: <http://landregistry.data.gov.uk/def/ppi/>\n" +
+                "\n" +
+                "# Returns the Price Paid data from the default graph for each transaction record having\n" +
+                "# an address with the given postcode.\n" +
+                "# The postcode to query is set using SPARQL 1.1's 'values' clause\n" +
+                "\n" +
+                "SELECT ?paon ?saon ?street ?town ?county ?postcode ?amount ?date ?category\n" +
+                "WHERE\n" +
+                "{\n" +
+                "  VALUES ?postcode {\"" + postcode + "\"^^xsd:string}\n" +
+                "\n" +
+                "  ?addr lrcommon:postcode ?postcode.\n" +
+                "\n" +
+                "  ?transx lrppi:propertyAddress ?addr ;\n" +
+                "          lrppi:pricePaid ?amount ;\n" +
+                "          lrppi:transactionDate ?date ;\n" +
+                "          lrppi:transactionCategory/skos:prefLabel ?category.\n" +
+                "\n" +
+                "  OPTIONAL {?addr lrcommon:county ?county}\n" +
+                "  OPTIONAL {?addr lrcommon:paon ?paon}\n" +
+                "  OPTIONAL {?addr lrcommon:saon ?saon}\n" +
+                "  OPTIONAL {?addr lrcommon:street ?street}\n" +
+                "  OPTIONAL {?addr lrcommon:town ?town}\n" +
+                "}\n" +
+                "ORDER BY ?amount";
+
+        JSONObject queryResponse = executeSPARQLQuery(postcodeQuery);
+
+        return  (ArrayNode) new ObjectMapper().readTree(
+                queryResponse.get("result").toString())
+                .get("results").get("bindings");
+
+    }
+
+
+    private JSONObject executeSPARQLQuery(String query) throws UnirestException, IOException {
+        //Navigates through JSON and returns list of addresses based on post code
+        return Unirest.post(LAND_REGISTRY_SPARQL_ENDPOINT)
+                .field("output", "json")
+                .field("q", query)
+                .field("url", "/landregistry/query")
+                .asJson()
+                .getBody()
+                .getObject();
+    }
 
 }
