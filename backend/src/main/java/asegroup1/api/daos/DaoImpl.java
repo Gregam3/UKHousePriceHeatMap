af4ea864493969461124cf437a5a651c5e949fd1
Greg <gregoryamitten@gmail.com>
2018-11-07 10:46:15 +0000

    Made entitymanager thread-safe

--- backend/src/main/java/asegroup1/api/daos/DaoImpl.java
+++ backend/src/main/java/asegroup1/api/daos/DaoImpl.java
@@ -3,9 +3,7 @@ package asegroup1.api.daos;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
+import javax.persistence.*;
 import java.util.List;
 
 /**
@@ -17,55 +15,69 @@ import java.util.List;
 @Repository
 public class DaoImpl<T> implements Dao<T> {
 
-	/**
-	 * A necessary parameter in many of the entityManager's methods
-	 */
-	private Class currentClass;
+    /**
+     * A necessary parameter in many of the entityManager's methods
+     */
+    private Class currentClass;
 
-	protected void setCurrentClass(Class<T> currentClass) {
-		this.currentClass = currentClass;
-	}
+    protected void setCurrentClass(Class<T> currentClass) {
+        this.currentClass = currentClass;
+    }
 
-	@PersistenceContext(type = PersistenceContextType.EXTENDED)
-	private EntityManager entityManager;
 
-	@SuppressWarnings("unchecked")
-	public T get(String id) {
-		checkIfCurrentClassIsValid();
-		return (T) entityManager.find(currentClass, id);
-	}
+    private static EntityManagerFactory entityManagerFactory;
 
-	//In hibernate, you query tables by there class name. This retrieves that.
-	private String retrieveHibernateTableNameFromClassString() {
-		String[] classNameSplitIntoPackageNames = currentClass.getName().split("\\.");
 
-		//Get the final element in array e.g. java.lang.Object
-		//                                                 ^
-		return classNameSplitIntoPackageNames[classNameSplitIntoPackageNames.length - 1];
-	}
+    //Set up entity manager based on properties provided in application.properties
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
 
-	public void delete(String id) {
-		entityManager.remove(get(id));
-	}
+    //Each time a Dao wants to access the database they fetch a new entity manager
+    private EntityManager getEntityManager() {
+        //On the first attempt to get an entity manager the factory creates a new one based on the properties in the template entity manager
+        if (entityManagerFactory == null)
+            entityManagerFactory = entityManager.getEntityManagerFactory();
 
-	public void update(T t) {
-		entityManager.merge(t);
-	}
+        return entityManagerFactory.createEntityManager();
+    }
 
-	@SuppressWarnings("unchecked")
-	public List<T> list() {
-		checkIfCurrentClassIsValid();
-		return entityManager.createQuery("from " + retrieveHibernateTableNameFromClassString(), currentClass).getResultList();
-	}
+    @SuppressWarnings("unchecked")
+    public T get(String id) {
+        checkIfCurrentClassIsValid();
+        return (T) getEntityManager().find(currentClass, id);
+    }
 
-	private void checkIfCurrentClassIsValid() {
-		if (currentClass == null) {
-			throw new AssertionError("In order to use this method you must set the class with setCurrentClass(). " +
-					"E.g. for UserDaoImpl extends DaoImpl<User>, you should have setCurrentClass(User.class) in your constructor.");
-		}
-	}
+    //In hibernate, you query tables by there class name. This retrieves that.
+    private String retrieveHibernateTableNameFromClassString() {
+        String[] classNameSplitIntoPackageNames = currentClass.getName().split("\\.");
 
-	public void add(T t) {
-		entityManager.persist(t);
-	}
+        //Get the final element in array e.g. java.lang.Object
+        //                                                 ^
+        return classNameSplitIntoPackageNames[classNameSplitIntoPackageNames.length - 1];
+    }
+
+    public void delete(String id) {
+        getEntityManager().remove(get(id));
+    }
+
+    public void update(T t) {
+        getEntityManager().merge(t);
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> list() {
+        checkIfCurrentClassIsValid();
+        return getEntityManager().createQuery("from " + retrieveHibernateTableNameFromClassString(), currentClass).getResultList();
+    }
+
+    private void checkIfCurrentClassIsValid() {
+        if (currentClass == null) {
+            throw new AssertionError("In order to use this method you must set the class with setCurrentClass(). " +
+                    "E.g. for UserDaoImpl extends DaoImpl<User>, you should have setCurrentClass(User.class) in your constructor.");
+        }
+    }
+
+    public void add(T t) {
+        getEntityManager().persist(t);
+    }
 }
