44973f223d1b5615382c5d3307b5c90053834d32
Greg <gregoryamitten@gmail.com>
2018-11-12 13:44:12 +0000

    Attempting to configure dao tests

---
+++ backend/src/main/java/asegroup1/api/configs/MySQLTestConfig.java
@@ -0,0 +1,38 @@
+package asegroup1.api.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author Greg Mitten
+ * gregoryamitten@gmail.com
+ */
+@Configuration
+@EnableJpaRepositories(basePackages = {
+        "asegroup1.api.daos"
+})
+@EnableTransactionManagement
+public class MySQLTestConfig {
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+
+        return dataSource;
+    }
+
+    // configure entityManagerFactory
+    // configure transactionManager
+    // configure additional Hibernate properties
+}
--- backend/src/test/asegroup1/api/daos/landregistry/LandRegistryDaoImplTest.java
+++ backend/src/test/asegroup1/api/daos/landregistry/LandRegistryDaoImplTest.java
@@ -1,31 +1,35 @@
 package asegroup1.api.daos.landregistry;
 
 import asegroup1.api.Application;
+import asegroup1.api.daos.DaoImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.applet.AppletContext;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * @author Greg Mitten
  * gregoryamitten@gmail.com
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
 @SpringBootTest(classes = Application.class)
 class LandRegistryDaoImplTest {
     @Autowired
-    LandRegistryDaoImpl landRegistryDao;
+    private LandRegistryDaoImpl landRegistryDao;
+
+    @Autowired
+    private DaoImpl dao;
 
     @Test
     public void test() {
-        landRegistryDao.searchForPostCodesInBoundaries(0,0,0,0);
+
+        assert dao != null;
+//        landRegistryDao.searchForPostCodesInBoundaries(0,0,0,0);
+        assert landRegistryDao != null;
     }
 
 }
