8a39e9ece92ff02ce046fbec928cf2f5adf72667
Rikkey <rikkeypaal@gmail.com>
2018-11-25 22:24:51 +0000

    Added constraints to add locaiton data

--- backend/src/main/java/asegroup1/api/controllers/LocationController.java
+++ backend/src/main/java/asegroup1/api/controllers/LocationController.java
@@ -1,14 +1,21 @@
 package asegroup1.api.controllers;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import asegroup1.api.models.LocationData;
 import asegroup1.api.services.location.LocationServiceImpl;
+import asegroup1.api.services.user.UserServiceImpl;
 
 /**
  * @author James Fernando, Rikkey Paal
@@ -18,17 +25,28 @@ import asegroup1.api.services.location.LocationServiceImpl;
 @RequestMapping("/location/")
 public class LocationController {
 
+	private UserServiceImpl userService;
     private LocationServiceImpl locationService;
 
     @Autowired
-    public LocationController(LocationServiceImpl locationService) {
+	public LocationController(LocationServiceImpl locationService, UserServiceImpl userServiceImpl) {
         this.locationService = locationService;
+		this.userService = userServiceImpl;
     }
 
     @PostMapping(value = {"add-location-data", "add-location-data/"})
     public ResponseEntity<String> postAddLocationData(@RequestBody LocationData location) {
-        locationService.create(location);
-        return new ResponseEntity<>("Successfully added to database", HttpStatus.OK);
+		if (userService.get(location.getUserId()) != null) {
+			try {
+				locationService.create(location);
+				return new ResponseEntity<>("Successfully added to database", HttpStatus.OK);
+			} catch (InvalidParameterException e) {
+				return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			}
+		} else {
+			return new ResponseEntity<String>("Could not add to database: User does not exist", HttpStatus.BAD_REQUEST);
+		}
+
     }
 
     @GetMapping(value = {"get-user-locations/{user-id}"})
