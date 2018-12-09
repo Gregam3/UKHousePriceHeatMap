ac513d5176e2ea303f4a8584362dbb85db7d2de1
Unknown <jamesfernando@hotmail.co.uk>
2018-12-09 01:31:31 +0000

    Added changes to Colour.java

--- backend/src/main/java/asegroup1/api/models/heatmap/Colour.java
+++ backend/src/main/java/asegroup1/api/models/heatmap/Colour.java
@@ -1,8 +1,8 @@
 package asegroup1.api.models.heatmap;
 
-import java.security.InvalidParameterException;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.security.InvalidParameterException;
 
 /**
  * @author Greg Mitten
@@ -12,7 +12,7 @@ public class Colour {
     private int red;
     private int green;
 
-    public Colour(int colourValue) {
+    public Colour(int colourValue) throws InvalidParameterException{
         int val1 = colourValue * 3;
 
         if (val1 > 400) setRed(colourValue);
@@ -22,7 +22,21 @@ public class Colour {
         } else if (val1 < 200) setGreen((int) (colourValue * 3.85));
     }
 
-    public void setGreen(int green) {
+    /**
+     * Sets up the colours if the red and green values are already set
+     * @param red the integer value for red between 0 and 255
+     * @param green the integer value for green between 0 and 255
+     * @throws InvalidParameterException if the values are between 0 and 255 this exception is thrown
+     */
+    public Colour(int red, int green) throws InvalidParameterException{
+        setRed(red);
+        setGreen(green);
+    }
+
+    public void setGreen(int green) throws InvalidParameterException{
+        if (green > 255 || green < 0) {
+            throw new InvalidParameterException("Colour value must be between 0-255");
+        }
         this.green = green;
     }
 
@@ -36,8 +50,11 @@ public class Colour {
         return red;
     }
 
-    public void setRed(int green) {
-        this.red = green;
+    public void setRed(int red) throws InvalidParameterException{
+        if (red > 255 || red < 0) {
+            throw new InvalidParameterException("Colour value must be between 0-255");
+        }
+        this.red = red;
     }
 
     @JsonIgnore
@@ -59,7 +76,7 @@ public class Colour {
         return "#" + toHexColourString(getRed()) + toHexColourString(getGreen()) + toHexColourString(getBlue());
     }
 
-    private String toHexColourString(int value) {
+    private String toHexColourString(int value) throws InvalidParameterException{
         if (value > 255 || value < 0) {
             throw new InvalidParameterException("Colour value must be between 0-255");
         }
