82c535d47688866ae567b871302bce8766fa3dc0
Greg <gregoryamitten@gmail.com>
2018-12-09 20:19:34 +0000

    Added testing on heatmap

--- backend/src/test/java/asegroup1/api/models/heatmap/HeatMapDataPointTest.java
+++ backend/src/test/java/asegroup1/api/models/heatmap/HeatMapDataPointTest.java
@@ -10,5 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class HeatMapDataPointTest {
 	@Test
-	void testHeatMapDataPointPOJO() {}
+	void testHeatMapDataPointPOJO() {
+		HeatMapDataPoint heatMapDataPoint =
+			new HeatMapDataPoint(0, 0, new Colour(255), 1);
+
+		heatMapDataPoint.setLatitude(1);
+		heatMapDataPoint.setLongitude(1);
+		heatMapDataPoint.setColour(new Colour(55));
+		heatMapDataPoint.setRadius(2);
+
+		assertEquals(1, heatMapDataPoint.getLatitude());
+		assertEquals(1, heatMapDataPoint.getLongitude());
+		assertEquals(new Colour(55).getHex(),
+					 heatMapDataPoint.getColour().getHex());
+		assertEquals(2, heatMapDataPoint.getRadius());
+
+		assert heatMapDataPoint.getId().length() == 36;
+	}
 }
